7c4d26ff7c61278f0c2039a428a4f31cb1858d7d
Joao <joakuster@gmail.com>
2017-05-03 23:47:38 -0300

    Add variable playground area

---
+++ variable-playground.js
@@ -0,0 +1,28 @@
+// var person = {
+//     name: 'Joaquín',
+//     age: 21
+// }
+
+// function updatePerson(obj) {
+//     // obj = {
+//     //     name: 'Joaquín',
+//     //     age: 24
+//     // };
+//     obj.age = 24;
+// }
+
+// updatePerson(person);
+// console.log(person);
+
+// Array Example
+var grades = [14, 37];
+
+function addGrade(gradesArr) {
+    gradesArr.push(35);
+    debugger;
+
+    // gradesArr = [14, 37, 35];
+}
+
+addGrade(grades);
+console.log(grades);
