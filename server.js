8c77c764be78d0e9efe94c5801617e8bac4120b4
Joao <joakuster@gmail.com>
2017-05-04 23:55:14 -0300

    Convert DELETE /todos/:id to sequelize

--- server.js
+++ server.js
@@ -52,15 +52,6 @@ app.get('/todos/:id', function(req, res){
     }, function (e) {
         res.status(500).send();
     });
-
-
-    // var matchedTodo = _.findWhere(todos, {id: todoId});
-
-    // if (matchedTodo) {
-    //     res.json(matchedTodo);
-    // } else {
-    //     res.status(404).send();
-    // }
 });
 
 // POST /todos/
@@ -77,14 +68,20 @@ app.post('/todos', function (req, res) {
 // DELETE /todos/:id
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {id: todoId});
-
-    if(!matchedTodo) {
-        res.status(404).json({"error": "no todo found with that id"});
-    } else {
-        todos = _.without(todos, matchedTodo);
-        res.json(matchedTodo);
-    }
+    
+    db.todo.destroy({
+        where: {
+            id: todoId
+        }
+    }).then(function (rowsDeleted) {
+        if (rowsDeleted === 0) {
+            res.status(404).json({"error": "no todo found with that id"});
+        } else {
+            res.status(204).send();
+        }
+    }, function (e) {
+        res.status(500).send();
+    });
 });
 
 // PUT /todos/:id
