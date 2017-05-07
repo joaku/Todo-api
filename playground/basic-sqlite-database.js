e480c8e70548bec3538427cc18427057a58a715b
Joao <joakuster@gmail.com>
2017-05-07 14:15:55 -0300

    Added todo/user association

--- playground/basic-sqlite-database.js
+++ playground/basic-sqlite-database.js
@@ -9,7 +9,7 @@ var Todo = sequelize.define('todo', {
         type: Sequelize.STRING,
         allowNull: false,
         validate: {
-            len: [1,250]
+            len: [1, 250]
         }
     },
     completed: {
@@ -19,46 +19,42 @@ var Todo = sequelize.define('todo', {
     }
 });
 
+var User = sequelize.define('user', {
+    email: {
+        type: Sequelize.STRING
+    }
+});
+
+Todo.belongsTo(User);
+User.hasMany(Todo);
 
 sequelize.sync({
-    //force: true
+    // force: true
 }).then(function () {
     console.log('Everything is synced');
 
-    return Todo.findById(3);
+    User.findById(1).then(function (user) {
+        user.getTodos({
+            where: {
+                completed: false
+            }
+        }).then(function (todos) {
+            todos.forEach(function(todo) {
+                console.log(todo.toJSON());
+            });
+        });
+    });
 
-    // Todo.create({
-    //     description: 'Take out trash'   
-    // }).then(function (todo) {
-    //     return Todo.create({
-    //         description: 'Clean office'
-    //     })
+    // User.create({
+    //     email: 'joakuster@gmail.com'
     // }).then(function () {
-    //     //return Todo.findById(1);
-    //     return Todo.findAll({
-    //         where: {
-    //             description: {
-    //                 $like: '%Office%'
-    //             }            
-    //         }
+    //     return Todo.create({
+    //         description: 'Clean yard'
     //     });
-    // }).then(function (todos) {
-    //     if(todos) {
-    //         todos.forEach(function(todo) {
-    //             console.log(todo.toJSON());
-    //         });
-    //     } else {
-    //         console.log('no todo found');
-    //     }
-    // }).catch(function (e) {
-    //     console.log(e);
+    // }).then(function (todo) {
+    //     User.findById(1).then(function (user) {
+    //         user.addTodo(todo);
+    //     });
     // });
-}).then(function (todo) {
-    if(todo) {
-        console.log(todo.toJSON());
-    } else {
-        console.log('no to do found');
-    }
-}).catch(function (error) {
-    console.log(error);
+
 });
